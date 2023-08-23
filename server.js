9c93aa7cbfa2ad467783dbed8b0a9893af65317d
Helena Pilon <138323083+hppilon@users.noreply.github.com>
2023-08-23 07:44:59 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Desenvolvimento de Aplicações WEB II" });
+});
+
+require("./routes/cachorro.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}.`);
+});
